506c970bc339d9d1bc576727f5e05445b349bf89
DeathCraftNetWork <44792516+DeathCraftNetWork@users.noreply.github.com>
2018-12-27 17:51:55 -0300

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -8,7 +8,7 @@ client.on("ready", () => {
    client.user.setPresence( {
        status: "online",
        game: {
-           name: "-help | Versión 1.1", "sdsa"
+           name: "-help | Versión 1.1",
            type: "PLAYING"
        }
    } );
