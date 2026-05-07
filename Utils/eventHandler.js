3046f9d76f08a0a69517d0d5039a5c5912919e91
JainaGam3r45 <dubecraft.original@gmail.com>
2026-05-07 14:25:45 -0300

    refactor(events): group handlers by Discord event

--- Utils/eventHandler.js
+++ Utils/eventHandler.js
@@ -2,31 +2,36 @@ async function loadEvents (client) {
     const { loadFiles } = require('../Functions/fileLoader');
     const logger = require("./logger");
 
+    client.eventDispatchers ??= new Map();
+
+    for (const dispatcher of client.eventDispatchers.values()) {
+        dispatcher.target.removeListener(dispatcher.name, dispatcher.execute);
+    }
+
+    client.eventDispatchers.clear();
     await client.events.clear();
 
     const files = await loadFiles("Events");
+    const eventGroups = new Map();
 
     for (const file of files) {
         try {
             const event = require(file);
-
-            const execute = async (...args) => {
-                try {
-                    await event.execute(...args, client);
-                } catch (error) {
-                    logger.error(`Event ${event.name} failed.`, error);
-                }
+            const groupKey = `${event.rest ? "rest" : "client"}:${event.name}`;
+            const group = eventGroups.get(groupKey) ?? {
+                name: event.name,
+                rest: Boolean(event.rest),
+                handlers: [],
             };
 
-            client.events.set(event.name, execute);
+            group.handlers.push({
+                file,
+                once: Boolean(event.once),
+                execute: event.execute,
+                ran: false,
+            });
 
-            if (event.rest) {
-                if (event.once) client.rest.once(event.name, execute);
-                else client.rest.on(event.name, execute);
-            } else {
-                if (event.once) client.once(event.name, execute);
-                else client.on(event.name, execute);
-            }
+            eventGroups.set(groupKey, group);
 
             logger.debug(`Loaded event ${event.name} from ${file}`);
         } catch (error) {
@@ -34,7 +39,48 @@ async function loadEvents (client) {
         }
     }
 
+    for (const group of eventGroups.values()) {
+        registerEventGroup(client, group, logger);
+    }
+
     logger.info("Events loading completed.");
+}
+
+function registerEventGroup(client, group, logger) {
+    const target = group.rest ? client.rest : client;
+    const shouldRunOnce = group.handlers.every((handler) => handler.once);
+    const execute = async (...args) => {
+        for (const handler of group.handlers) {
+            if (handler.once && handler.ran) continue;
+
+            try {
+                handler.ran = true;
+                await handler.execute(...args, client);
+            } catch (error) {
+                logger.error(`Event ${group.name} failed in ${handler.file}.`, error);
+            }
+        }
+    };
+
+    client.events.set(group.name, {
+        execute,
+        handlers: group.handlers,
+        rest: group.rest,
+    });
+
+    client.eventDispatchers.set(`${group.rest ? "rest" : "client"}:${group.name}`, {
+        target,
+        name: group.name,
+        execute,
+    });
+
+    if (shouldRunOnce) {
+        target.once(group.name, execute);
+    } else {
+        target.on(group.name, execute);
+    }
+
+    logger.debug(`Registered ${group.handlers.length} handler(s) for ${group.name}.`);
 }
 
 module.exports = { loadEvents };
